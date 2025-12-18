25e4e4b2a09dccc616c90816afb8c6eaafa84158
bala-amypo <bala@amypo.in>
2025-12-18 09:10:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/DigitalKeyRepository.java
+++ src/main/java/com/example/demo/repository/DigitalKeyRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface DigitalKeyRepository extends JpaRepository<DigitalKey, Long> {
+public interface DigitalKeyRepository extends JpaRepository<DigitalKey,Long> {
     Optional<DigitalKey> findByBookingIdAndActiveTrue(Long bookingId);
     List<DigitalKey> findByBookingGuestId(Long guestId);
 }
