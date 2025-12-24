08dd90d93bac72f5b1f9441df3e49edaecd83b25
bala-amypo <bala@amypo.in>
2025-12-24 06:46:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -68,6 +68,7 @@ public class AuthController {
         }
 
         if (!passwordEncoder.matches(request.getPassword(), guest.getPassword())) {
+                System.out.println(passwordEncoder.matches(request.getPassword(), guest.getPassword()));
             return ResponseEntity.badRequest()
                     .body(new ApiResponse(false, "Invalid credentials"));
         }
