3d293b8207244af4b02003919a919554c929379b
bala-amypo <bala@amypo.in>
2025-12-25 16:57:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/GuestService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Guest;
+import java.util.List;
+
+public interface GuestService {
+    Guest createGuest(Guest guest);
+    Guest updateGuest(Long id, Guest guest);
+    Guest getGuestById(Long id);
+    Guest getGuestByEmail(String email);
+    List<Guest> getAllGuests();
+    void deactivateGuest(Long id);
+}
