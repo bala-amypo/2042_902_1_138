eda6ca09e096b2e89c84e0301424a27edf9e8a46
bala-amypo <bala@amypo.in>
2025-12-28 15:31:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Guest.java
+++ src/main/java/com/example/demo/model/Guest.java
@@ -1,3 +1,11 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Guest {
 
@@ -11,9 +19,55 @@ public class Guest {
     private String password;
     private String fullName;
     private String phoneNumber;
-    private String role = "ROLE_USER";
     private Boolean active = true;
-    private Boolean verified = false;
 
-    // getters + setters
+    public Guest() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
