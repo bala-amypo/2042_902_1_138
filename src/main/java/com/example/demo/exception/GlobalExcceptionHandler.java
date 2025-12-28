9dfea618b10786c0969ba9c95e8a4c65b254e041
bala-amypo <bala@amypo.in>
2025-12-28 16:14:36 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExcceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExcceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExcceptionHandler { // Matching the filename typo
+public class GlobalExcceptionHandler { // Matches filename
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
